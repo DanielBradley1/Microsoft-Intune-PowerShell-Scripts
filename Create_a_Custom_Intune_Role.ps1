bce803d2b011bdf8cc3f1e449a01d532cca232bb
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2022-12-19 15:18:04 +0000

    Create Create_a_Custom_Intune_Role.ps1

---
+++ Create_a_Custom_Intune_Role.ps1
@@ -0,0 +1,66 @@
+###
+$DisplayName = "My Custom Role2" #Define your role name
+$RoleDescription = "My custom role description2" #Define the role description
+###
+
+#Import module
+Import-Module Microsoft.Graph.DeviceManagement.Administration
+
+#Select beta profile
+Select-MgProfile -Name Beta
+
+#Connect to Microsoft Graph
+Connect-MgGraph -scopes DeviceManagementRBAC.ReadWrite.All
+
+#Store the URI path
+$uri = "https://graph.microsoft.com/beta/deviceManagement/roleDefinitions/"
+
+#Store Json Payload
+$json = @'
+		{
+    "@odata.context": "https://graph.microsoft.com/beta/$metadata#deviceManagement/roleDefinitions/$entity",
+    "@odata.type": "#microsoft.graph.deviceAndAppManagementRoleDefinition",
+    "displayName": "<rname>",
+    "description": "<rdesc>",
+    "isBuiltInRoleDefinition": true,
+    "isBuiltIn": true,
+    "roleScopeTagIds": [],
+    "permissions": [
+        {
+            "actions": [
+                "Microsoft.Intune_Organization_Read",
+            ],
+            "resourceActions": [
+                {
+                    "allowedResourceActions": [
+                        "Microsoft.Intune_ManagedDevices_Read",
+                    ],
+                    "notAllowedResourceActions": []
+                }
+            ]
+        }
+    ],
+    "rolePermissions": [
+        {
+            "actions": [
+                "Microsoft.Intune_ManagedDevices_Read",
+            ],
+            "resourceActions": [
+                {
+                    "allowedResourceActions": [
+                        "Microsoft.Intune_ManagedDevices_Read",
+                    ],
+                    "notAllowedResourceActions": []
+                }
+            ]
+        }
+    ]
+}
+'@
+
+#Update unique payload information
+$json = $json -replace '<rname>',$DisplayName
+$json = $json -replace '<rdesc>',$RoleDescription
+
+#Create custom Intune role
+Invoke-MgGraphRequest -Uri $uri -body $json -Method POST -ContentType "application/json"
