d2df628bb089899a81a7ce44d2eed9523493f126
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2023-12-15 05:07:26 +0000

    Update Bulk_On_Demand_Remediation_Packages.ps1

--- Remediation/Bulk_On_Demand_Remediation_Packages.ps1
+++ Remediation/Bulk_On_Demand_Remediation_Packages.ps1
@@ -1,33 +1,24 @@
 #Connect to Microsoft Graph
 Connect-MgGraph -scopes DeviceManagementConfiguration.Read.All, DeviceManagementManagedDevices.Read.All, DeviceManagementManagedDevices.PrivilegedOperations.All
+
 Install-Module Microsoft.Graph.Beta.DeviceManagement -scope CurrentUser -Force
 Import-Module Microsoft.Graph.Beta.DeviceManagement
 
-
 #Get Script package
 $ScriptPackageName = "Local Admin Accounts (for LAPS)"
-$RemediationPachage = Get-MgBetaDeviceManagementDeviceHealthScript -Filter "DisplayName eq '$ScriptPackageName'" -ExpandProperty assignments
+$RemediationPachage = Get-MgBetaDeviceManagementDeviceHealthScript -Filter "DisplayName eq '$ScriptPackageName'"
 
-#Evaluate membership
-If ($RemediationPachage.Assignments.target.AdditionalProperties.Keys -match "groupId") {
-    $switch = "group"
-    $assignments = $RemediationPachage.Assignments.target.AdditionalProperties.groupId
-} Else {
+#Create request body
+$body = @{
+    "ScriptPolicyId" = "78c339b9-bc38-49f6-81a9-cab9bffe18ed"
+} | ConvertTo-Json
 
+#Store target devices
+$TargetDevices = Get-MgBetaDeviceManagementManagedDevice -filter "OwnerType eq 'Company'"
 
+#Loop through each device
+Foreach ($device in $TargetDevices){
+    Write-Host "Initiating remediation package $ScriptPackageName for $($Device.DeviceName)" -ForegroundColor Cyan
+    $uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices('$($device.id)')/initiateOnDemandProactiveRemediation"
+    Invoke-MgGraphRequest -Uri $uri -Method POST -Body $body -ContentType  "application/json"
 }
-
-If ($switch -eq "group") {
-    $groupmembers = @()
-    Foreach ($group in $assignments) {}
-        Get-MgBetaGroupMember -GroupId $group
-
-    }
-}
-
-
-
-https://graph.microsoft.com/beta/deviceManagement/managedDevices('5dcca03d-635c-432f-8534-131ee152cc85')/initiateOnDemandProactiveRemediation
-
-
-Get-Mg
