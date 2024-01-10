34c26df0ec0b81d101408c7498344d1f74f5a688
dbradley-xi <96792909+dbradley-xi@users.noreply.github.com>
2024-01-10 15:17:23 +0000

    updated

--- Microsoft 365/Deploy M365/Detect-M365.ps1
+++
@@ -1,8 +0,0 @@
-$apps = (Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall") |  Get-ItemProperty | Where {$_.DisplayName -match "Microsoft 365 "}
-if ($apps) {
-    Write-host "M365 Apps Detected"
-	Exit 0
-   }else{
-    Write-host "M365 Apps not Detected"
-    Exit 1
-}
